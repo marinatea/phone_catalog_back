beb3cee06e850db7ff1ff72facca8a9e687960e2
Maryna Karpova <mari.artarea@gmail.com>
2024-08-20 17:12:33 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,17 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      'no-undef': 'off',
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-undef": "off",
+      "@typescript-eslint/no-var-requires": "off",
     },
   },
 ];
