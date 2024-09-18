81b0de69a80f4dfa7d81f69cb465a041cf76ce8e
Maryna Karpova <mari.artarea@gmail.com>
2024-09-18 16:04:59 +0200

    .

--- dist/routes/accessories.router.ts
+++
@@ -1,9 +0,0 @@
-import accessoryController from '../controllers/accessories.controller';
-import express from 'express';
-
-const accessoryRouter = express.Router();
-
-accessoryRouter.get('/', accessoryController.getAll);
-accessoryRouter.get('/:accessoryId', accessoryController.getById);
-
-export default accessoryRouter;
--- dist/routes/tablets.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import tabletController from '../controllers/tablets.controller';
-
-const tabletRouter = express.Router();
-
-tabletRouter.get('/', tabletController.getAll);
-tabletRouter.get('/:tabletId', tabletController.getById);
-
-export default tabletRouter;
